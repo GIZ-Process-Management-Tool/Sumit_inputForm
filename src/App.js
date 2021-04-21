cf7a7b4748eacd3f743e2fe7a9a525fb091fa74e
sumitkumar105 <72189267+sumitkumar105@users.noreply.github.com>
2021-04-21 22:01:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./Navbar/Nav";
+import Service1 from "./Pages/Service1";
+import Service2 from "./Pages/Service2";
+import Demo from "./Pages/Demo";
+import Order from "./Input_forms/Order";
+import Loomloadingchart from "./Input_forms/Loomloadingchart";
+import LoomperDay from "./Input_forms/LoomPerDay";
+import Shift from "./Input_forms/Shift";
+import GreyReport from "./Input_forms/GreyReport";
+import Repairing from "./Input_forms/Repairing";
+import DownTime from "./Input_forms/Dwontime";
+import Demo2 from "./Input_forms/Demo2";
+import Worker from "././loginForm/Worker";
+import Admin from "././loginForm/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Service1} />
+        <Route path="/service2" component={Service2} />
+        <Route path="/admin" component={Admin} />
+        <Route path="/worker" component={Worker} />
+        <Route path="/demo" component={Demo2} />
+        <Route path="/order" component={Order} />
+        <Route path="/loomloadingchart" component={Loomloadingchart} />
+        <Route path="/loomperday" component={LoomperDay} />
+        <Route path="/shift" component={Shift} />
+        <Route path="/greyReport" component={GreyReport} />
+        <Route path="/repairing" component={Repairing} />
+        <Route path="/downtime" component={DownTime} />
+      </Switch>
+    </>
   );
 }
 
